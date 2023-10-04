ad8f985a1029444ef81c43231826634f031b9e65
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-04 15:30:24 +0200

    Outputting HTML

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = Vue.createApp({
     data() {
         return {
             courseGoalA: 'Finish the course and learn Vue!',
-            courseGoalB: 'Master Vue and find work as Vue Developer!',
+            courseGoalB: '<h3>Master Vue and find work as Vue Developer!</h3>',
             vueLink: 'https://vuejs.org',
             yes: 'Yes',
             no: 'No',
