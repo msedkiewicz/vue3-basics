a49edeceb9c0185ac106d6411ea4bb41680a096c
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-04 15:06:06 +0200

    Add method and method call

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             courseGoal: 'Finish the course and learn Vue!',
-            vueLink: 'https://vuejs.org'
+            vueLink: 'https://vuejs.org',
+            yes: 'Yes',
+            no: 'No',
+            myVar: 2
         };
     },
     methods: {
         outputGoal() {
-
+            const randomNumber = Math.random();
+            if (randomNumber < 0.5) {
+                return 'Learn Vue!';
+            } else {
+                return 'Master Vue!';
+            }
         }
     }
 });
