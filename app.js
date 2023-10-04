0bbc02c5a1451d64b61a239e3140d22788b3157b
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-04 15:13:39 +0200

    Different syntax for outputing data

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: 'Finish the course and learn Vue!',
+            courseGoalA: 'Finish the course and learn Vue!',
+            courseGoalB: 'Master Vue and find work as Vue Developer!',
             vueLink: 'https://vuejs.org',
             yes: 'Yes',
             no: 'No',
@@ -12,9 +13,9 @@ const app = Vue.createApp({
         outputGoal() {
             const randomNumber = Math.random();
             if (randomNumber < 0.5) {
-                return 'Learn Vue!';
+                return this.courseGoalA;
             } else {
-                return 'Master Vue!';
+                return this.courseGoalB;
             }
         }
     }
