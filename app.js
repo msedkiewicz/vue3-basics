be4726ca04f349b04333a10ae5bf06e7006efa19
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-09 20:09:18 +0200

    Different solution

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const app = Vue.createApp({
             myAge: 25,
             imgSunrise: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2019/09/epic-sunrise-photos-fog.jpg?fit=1500%2C1001&ssl=1',
         }
+    },
+    methods: {
+        ageInFiveYears() {
+            return this.myAge + 5;
+        },
+        randomNumber() {
+            return Math.random();
+        }
     }
 });
 
