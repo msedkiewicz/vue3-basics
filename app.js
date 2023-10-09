f979573413cf98c47f6061c54e8881bc23edb59a
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-09 20:04:13 +0200

    Add image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = Vue.createApp({
         return {
             myName: 'Lena',
             myAge: 25,
+            imgSunrise: 'https://i0.wp.com/digital-photography-school.com/wp-content/uploads/2019/09/epic-sunrise-photos-fog.jpg?fit=1500%2C1001&ssl=1',
         }
     }
 });
