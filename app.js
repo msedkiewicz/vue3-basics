0997a6e03c0c11d4ad152cffde032e6e52bf96f6
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-18 22:19:03 +0200

    Not efficient watcher example

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
-      name: ''
+      name: '',
+      fullname: '',
     };
   },
-  computed: { // recalculate when dependency change
-    fullName() {
-      console.log('Test')
+  watch: {
+    name(value) {
       if (this.name === '') {
-        return '';
-      }
-      return this.name + ' ' + 'Schwarzmueller';
+        this.fullname = '';
+      } else {
+        this.fullname = value + ' ' + 'Schwarzmueller';
+      } // [problematic with more complex data]
     }
+  },
+  computed: { // recalculate when dependency change
+    // fullname() {
+    //   console.log('Test')
+    //   if (this.name === '') {
+    //     return '';
+    //   }
+    //   return this.name + ' ' + 'Schwarzmueller';
+    // }
   },
   methods: { // use only if you want to recalculate values on the page whenever anything change on page + in case of events
     setName(event, lastName) {
