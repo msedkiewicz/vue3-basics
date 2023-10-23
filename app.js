c0db18228a31d18f682996f9ff12d63dc9dfbd60
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-23 22:36:34 +0200

    Add addNumber generic function

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+    data() {
+        return {
+            counter: 0,
+        };
+    },
+    methods: {
+        addNumber(value) {
+            this.counter = this.counter + value;
+            console.log(this.counter);
+        }
+    }
+});
 
 app.mount('#assignment');
