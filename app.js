7c5ae615eb26caea54309aa7721d951d7e87317d
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-23 22:37:27 +0200

    Code formatting

--- app.js
+++ app.js
@@ -1,15 +1,14 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            counter: 0,
-        };
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    addNumber(value) {
+      this.counter = this.counter + value;
     },
-    methods: {
-        addNumber(value) {
-            this.counter = this.counter + value;
-            console.log(this.counter);
-        }
-    }
+  },
 });
 
-app.mount('#assignment');
+app.mount("#assignment");
