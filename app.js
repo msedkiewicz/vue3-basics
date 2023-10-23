c845728967c6cd0c4f658d14972a4df9cf269857
msedkiewicz <magdalenainspirations@gmail.com>
2023-10-23 22:42:01 +0200

    Add result description

--- app.js
+++ app.js
@@ -4,9 +4,21 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  computed: {
+    result() {
+      if (this.counter < 37) {
+        return "Not there yet!";
+      } else if (this.counter === 37) {
+        return this.counter;
+      } else {
+        return "Too much!";
+      }
+    },
+  },
   methods: {
     addNumber(value) {
       this.counter = this.counter + value;
+    //   console.log(this.counter);
     },
   },
 });
