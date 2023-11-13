d75bc213228f35dcd82a3c046724713c3dfe959c
msedkiewicz <magdalenainspirations@gmail.com>
2023-11-13 21:11:41 +0100

    Add toggle; move classes code to computed property

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ const app = Vue.createApp({
     data() {
         return {
             inputClass: '',
-
+            isVisible: true,
+        }
+    },
+    computed: {
+        addedClasses() {
+            return {user1: this.inputClass ==='user1', user2: this.inputClass ==='user2',
+            visible: this.isVisible,
+            hidden: !this.isVisible
+            }
+        }
+    },
+    methods: {
+        toggleVisibility() {
+            this.isVisible = !this.isVisible;
         }
     }
 });
