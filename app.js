074d8f07b96dcbb9670982ca419a2f9549d03e14
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-08 21:17:55 +0100

    Multiple Vue apps instances

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
-// const app = Vue.createApp({
-//   data() {
-//     return {
-//       currentUserInput: '',
-//       message: 'Vue is great!',
-//     };
-//   },
-//   methods: {
-//     saveInput(event) {
-//       this.currentUserInput = event.target.value;
-//     },
-//     setText() {
-//       this.message = this.currentUserInput;
-//     },
-//   },
-// });
+const app = Vue.createApp({
+  data() {
+    return {
+      currentUserInput: '',
+      message: 'Vue is great!',
+    };
+  },
+  methods: {
+    saveInput(event) {
+      this.currentUserInput = event.target.value;
+    },
+    setText() {
+      this.message = this.currentUserInput;
+    },
+  },
+});
 
-// app.mount('#app');
+app.mount('#app');
 
-// vanilla JS is not reactive by default
-
-const data = {
-  message: 'Hello!',
-  longMessage: 'Hello! World!'
-};
-
-const handler = {
-  set(target, key, value) {
-    if (key === 'message') {
-      target.longMessage = value + 'World!';
+const app2 = Vue.createApp({
+  data() {
+    return {
+      favoriteMeal: 'lasagna'
     }
-    target.message = value;
   }
-}
-
-const proxy = new Proxy(data, handler);
-
-proxy.message = 'Helloooo!!!!';
-
-console.log(proxy.longMessage);
+})
+app2.mount('#app2');
