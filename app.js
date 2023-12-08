b0f64a489bbed87e8c05b8ff66130bae5341b1b4
msedkiewicz <magdalenainspirations@gmail.com>
2023-12-08 20:51:43 +0100

    Example of Vanilla JS being not reactive

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      this.message = this.currentUserInput;
-    },
-  },
-});
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       currentUserInput: '',
+//       message: 'Vue is great!',
+//     };
+//   },
+//   methods: {
+//     saveInput(event) {
+//       this.currentUserInput = event.target.value;
+//     },
+//     setText() {
+//       this.message = this.currentUserInput;
+//     },
+//   },
+// });
 
-app.mount('#app');
+// app.mount('#app');
+
+// vanilla JS is not reactive by default
+
+let message = 'Hello!';
+
+let longMessage = message + ' World!';
+
+console.log(longMessage);
+
+message = 'Hello';
+
+console.log(longMessage);
